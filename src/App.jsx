bda0e439ba6df40e8644ec2909873528f03c3c7f
unknown <wiraprathamaalvin@gmail.com>
2024-02-20 15:12:21 +0800

    add login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
 
   return (
       <div>
-        <Register />
+        <Login />
       </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,119 @@
-import { Box, Card, CardBody, Text, Input, Flex } from '@chakra-ui/react';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import {
+  Card,
+  CardBody,
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Icon,
+  Input,
+  Link,
+  Switch,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import loginImage from '../assets/loginimage.png';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
+  const bgForm = useColorModeValue('white', 'navy.800');
+  const titleColor = useColorModeValue('gray.700', 'blue.500');
+  const textColor = useColorModeValue('gray.700', 'white');
+  const colorIcons = useColorModeValue('gray.700', 'white');
+  const bgIcons = useColorModeValue('transparent', 'navy.700');
+  const bgIconsHover = useColorModeValue('gray.50', 'whiteAlpha.100');
+
   return (
-    <Flex 
-    align="center" 
-    justify="center" 
-    height="100vh" 
-    bgImage={`url(${loginImage})`} 
-    backgroundSize="cover"
-    backgroundPosition="center">
-      <Card w={452} h={629}>
-        <CardBody mt={10}>
-          <Text fontSize="2xl" fontWeight="bold" mb={2} textAlign="center">
-            Sign In
+    <Flex
+      align="center"
+      justify="center"
+      height="100vh"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      flexDirection="column"
+    >
+      <img src={logo} alt="Logo Apps" style={{ width: '150px', marginBottom: '20px' }} />
+      <Box
+        position="absolute"
+        minH={{ base: '70vh', md: '50vh' }}
+        maxH={{ base: '70vh', md: '50vh' }}
+        w={{ md: 'calc(100vw - 50px)' }}
+        maxW={{ md: 'calc(100vw - 50px)' }}
+        left="0"
+        right="0"
+        bgRepeat="no-repeat"
+        overflow="hidden"
+        zIndex="-1"
+        top="0"
+        bgImage={loginImage}
+        bgSize="cover"
+        mx={{ md: 'auto' }}
+        mt={{ md: '14px' }}
+        borderRadius={{ base: '0px', md: '20px' }}
+      ></Box>
+      <Card w={452} h={629} display="flex" alignItems="center">
+        <CardBody w={350} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+          <Text
+            fontSize="xl"
+            color={textColor}
+            fontWeight="bold"
+            textAlign="center"
+            mb="22px"
+          >
+            Masuk
           </Text>
-          <Text fontSize="md" mb={4} textAlign="center">
-            Sign in as an admin to continue
-          </Text>
-
-          <Box mb={4}>
-            <Text>Username</Text>
-            <Input placeholder='Enter your username' />
-          </Box>
-
-          <Box mb={4}>
-            <Text>Password</Text>
-            <Input type='password' placeholder='Enter your password' />
-          </Box>
-
-          <Button mt={24} width='full' colorScheme='blue'>Button</Button>
-          <Text mt={4} textAlign="center">Don’t have account? Sign Up</Text>
+          <FormControl>
+            <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+              Username
+            </FormLabel>
+            <Input
+              variant="auth"
+              fontSize="sm"
+              ms="4px"
+              type="text"
+              placeholder="@johndoe"
+              mb="24px"
+              size="lg"
+            />
+            <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+              Sandi
+            </FormLabel>
+            <Input
+              variant="auth"
+              fontSize="sm"
+              ms="4px"
+              type="text"
+              placeholder="Masukkan sandi anda"
+              mb="24px"
+              size="lg"
+            />
+            <Button
+              fontSize="10px"
+              variant="dark"
+              fontWeight="bold"
+              w="100%"
+              h="45"
+              mb="24px"
+            >
+              Masuk
+            </Button>
+          </FormControl>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            maxW="100%"
+            mt="0px"
+          >
+            <Text color={textColor} fontWeight="medium">
+              Belum terdaftar sebelumnya?
+              <Link color={titleColor} as="span" ms="5px" href="#" fontWeight="bold">
+                Daftar
+              </Link>
+            </Text>
+          </Flex>
         </CardBody>
       </Card>
     </Flex>
